aaa04c9acbb7156e916cbb4973c2f70ee1de20cb
Adriano Costa <adrianofacosta@gmail.com>
2022-10-24 19:12:20 -0300

    :sparkles: feat: made createProduct method at ProductModel

---
+++ src/models/ProductModel.ts
@@ -0,0 +1,21 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { NewProduct } from '../interfaces/NewProduct';
+
+export default class ProductModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async createProduct(newProduct: Omit<NewProduct, 'id'>): Promise<NewProduct> {
+    const { name, amount } = newProduct;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...newProduct };
+  }
+}
