b872d823de6ba4d1563b91383f0644a462e3b760
Adriano Costa <adrianofacosta@gmail.com>
2022-10-24 23:11:29 -0300

    :feat: create GET:/products router

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -4,6 +4,7 @@ import ProductController from '../controllers/ProductController';
 const routers = Router();
 const productController = new ProductController();
 
+routers.get('/products', productController.getAll);
 routers.post('/products', productController.newProduct);
 
 export default routers;
