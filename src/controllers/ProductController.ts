12c52492e47be09d155716910e6d9d48f523755a
Adriano Costa <adrianofacosta@gmail.com>
2022-10-24 22:31:00 -0300

    :sparkles: feat: create newProduct method at ProductController

---
+++ src/controllers/ProductController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import statusCodes from '../../utils/statusCode';
+import ProductServices from '../services/ProductService';
+
+export default class ProductController {
+  constructor(private productService = new ProductServices()) { }
+
+  public newProduct = async (req: Request, res: Response) => {
+    const product = req.body;
+    const createdProduct = await this.productService.createProduct(product);
+    res.status(statusCodes.CREATED).json(createdProduct);
+  };
+}
