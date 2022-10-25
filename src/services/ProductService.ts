a59fa1624a84c5a96c814a353713871bb8b8af68
Adriano Costa <adrianofacosta@gmail.com>
2022-10-24 23:10:53 -0300

    :feat: create getAll method at ProductService

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,7 @@
 import connection from '../models/connection';
 import ProductModel from '../models/ProductModel';
 import NewProduct from '../interfaces/NewProduct';
+import Product from '../interfaces/Product';
 
 export default class ProductService {
   private model: ProductModel;
@@ -12,5 +13,10 @@ export default class ProductService {
   public async createProduct(newProduct: Omit<NewProduct, 'id'>): Promise<NewProduct> {
     const createdProduct = await this.model.createProduct(newProduct);
     return createdProduct;
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const products = await this.model.getAll();
+    return products;
   }
 }
