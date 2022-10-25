0a7260ee6d67be0d70ee7efe241625dc150836a3
Adriano Costa <adrianofacosta@gmail.com>
2022-10-25 17:14:30 -0300

    :sparkles: feat: setup app.ts to use errorMiddleware route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import httpErrorMiddleware from './middlewares/errorMiddleware';
 import productRouter from './routers/productRouter';
 import userRouter from './routers/userRouter';
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(productRouter);
 app.use(userRouter);
+app.use(httpErrorMiddleware);
 
 export default app;
