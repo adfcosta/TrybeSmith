134c804a817f48f93e8320c54e23f976ed539177
Adriano Costa <adrianofacosta@gmail.com>
2022-10-25 22:09:48 -0300

    :feat: create productValidation Middleware

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -7,11 +7,32 @@ const loginSchema = Joi.object({
   password: Joi.string().required(),
 });
 
+const productSchema = Joi.object({
+  name: Joi.string().required().min(3),
+  amount: Joi.string().required().min(3),
+
+});
+
 const loginValidation = async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
   const { error } = loginSchema.validate({
     username,
     password,
+  });
+  if (error) {
+    console.log(error.details[0].type);
+    const errorType = error.details[0].type;
+    const errorCode = statusSwitchCase(errorType);
+    return res.status(errorCode).json({ message: error.message });
+  }
+  next();
+};
+
+export const productValidation = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, amount } = req.body;
+  const { error } = productSchema.validate({
+    name,
+    amount,
   });
   if (error) {
     console.log(error.details[0].type);
