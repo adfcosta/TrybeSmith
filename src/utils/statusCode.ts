88037244c0ece4d13efc76d8e17fc582feab6795
Adriano Costa <adrianofacosta@gmail.com>
2022-10-25 21:59:13 -0300

    :sparkles: feat: create StatusSwitch case utils and expand statusCodes

--- src/utils/statusCode.ts
+++ src/utils/statusCode.ts
@@ -5,6 +5,21 @@ const statusCodes = {
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   NO_CONTENT: 204,
+  UNPROCESSABLE_ENTITY: 422,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export const statusSwitchCase = (type: string) => {
+  switch (type) {
+    case 'any.required':
+      return statusCodes.BAD_REQUEST;
+    case 'string.base':
+      return statusCodes.UNPROCESSABLE_ENTITY;
+    case 'string.min':
+      return statusCodes.UNPROCESSABLE_ENTITY;
+    default:
+      return statusCodes.INTERNAL_SERVER_ERROR;
+  }
 };
     
 export default statusCodes;
