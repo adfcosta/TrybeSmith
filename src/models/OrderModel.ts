b782d2b77640fbf780aba795f6d8130d5a8d7b63
Adriano Costa <adrianofacosta@gmail.com>
2022-10-26 00:30:26 -0300

    :sparkles: feat: create getAll method at OrderModel

---
+++ src/models/OrderModel.ts
@@ -0,0 +1,21 @@
+import { Pool } from 'mysql2/promise';
+import Order from '../interfaces/Order';
+
+export default class OrderModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Order[]> {
+    const result = await this.connection.execute(
+      `SELECT
+      O.id, U.id AS userId, JSON_ARRAYAGG(P.id) AS productsIds 
+      FROM Trybesmith.Orders AS O INNER JOIN  Trybesmith.Users AS U 
+      ON O.userId = U.id INNER JOIN Trybesmith.Products AS P ON P.orderId = O.id GROUP BY O.id`,
+    );
+    const [rows] = result;
+    return rows as Order[];
+  }
+}
