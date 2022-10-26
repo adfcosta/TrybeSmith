84750e5c29bba6ec9518cb58e019ef33542fe7be
Adriano Costa <adrianofacosta@gmail.com>
2022-10-26 16:34:16 -0300

    :sparkles: feat: create authTokenValidation method at JWT

--- src/utils/JWT.ts
+++ src/utils/JWT.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { UserToken } from '../interfaces/User';
+import HttpException from './http.exception';
 
 export default class Token {
   TOKEN_SECRET = process.env.JWT_SECRET as string;
@@ -8,5 +9,22 @@ export default class Token {
     const payload = { username, id };
     const token = jwt.sign(payload, this.TOKEN_SECRET);
     return token;
+  };
+
+  public authTokenValidation = async (token: string) => {
+    if (!token) {
+      const err: HttpException = new Error('Token not found');
+      err.status = 401;
+      throw err;
+    }
+
+    try {
+      const instrospection = jwt.verify(token, this.TOKEN_SECRET);
+      return instrospection as UserToken;
+    } catch (error) {
+      const err: HttpException = new Error('Invalid token');
+      err.status = 401;
+      throw err;
+    }
   };
 }
