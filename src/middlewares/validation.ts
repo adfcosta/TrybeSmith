69c77858ff755bfafeed1152ee6eef85acf8fbab
Adriano Costa <adrianofacosta@gmail.com>
2022-10-25 22:00:39 -0300

    :recycle: refactor: use statusSwitchCase for login validation

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,5 +1,6 @@
 import Joi from 'joi';
 import { NextFunction, Request, Response } from 'express';
+import { statusSwitchCase } from '../utils/statusCode';
 
 const loginSchema = Joi.object({
   username: Joi.string().required(),
@@ -13,8 +14,10 @@ const loginValidation = async (req: Request, res: Response, next: NextFunction) 
     password,
   });
   if (error) {
-    console.log(error.message);
-    return res.status(400).json({ message: error.message });
+    console.log(error.details[0].type);
+    const errorType = error.details[0].type;
+    const errorCode = statusSwitchCase(errorType);
+    return res.status(errorCode).json({ message: error.message });
   }
   next();
 };
