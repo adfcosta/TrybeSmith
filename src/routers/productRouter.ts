d2d70158bc946e0b4b22b7303f472a2ca56e13a1
Adriano Costa <adrianofacosta@gmail.com>
2022-10-25 22:10:52 -0300

    :sparkles: feat: setup productValidation at POST:/prodcuts router

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { productValidation } from '../middlewares/validation';
 
 const routers = Router();
 const productController = new ProductController();
 
 routers.get('/products', productController.getAll);
-routers.post('/products', productController.newProduct);
+routers.post('/products', productValidation, productController.newProduct);
 
 export default routers;
