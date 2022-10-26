a293263e8ff89bf15b6d4c0a73b5e7bebd231677
Adriano Costa <adrianofacosta@gmail.com>
2022-10-26 16:44:56 -0300

    :sparkles: feat: create GenerateOrder and Create method at OrderService

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -1,9 +1,12 @@
 import connection from '../models/connection';
 import OrderModel from '../models/OrderModel';
+import ProductService from './ProductService';
 import Order from '../interfaces/Order';
 
 export default class OrderService {
   private model: OrderModel;
+
+  private productService = new ProductService();
 
   constructor() {
     this.model = new OrderModel(connection);
@@ -12,5 +15,18 @@ export default class OrderService {
   public async getAll(): Promise<Order[]> {
     const orders = await this.model.getAll();
     return orders;
+  }
+
+  public async create(userId: number): Promise<number> {
+    const orderId = await this.model.create(userId);
+    return orderId;
+  }
+
+  public async generateOrder(userId: number, productsIds: number[]): Promise<object> {
+    const orderId = await this.create(userId);
+    productsIds.forEach(async (productId) => {
+      await this.productService.updateProductOrder(orderId, productId);
+    });
+    return { userId, productsIds };
   }
 }
