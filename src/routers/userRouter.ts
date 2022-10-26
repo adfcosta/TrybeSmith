8a90b4c7670248098ed49a5d45a55b5369d91ed4
Adriano Costa <adrianofacosta@gmail.com>
2022-10-25 23:04:10 -0300

    :sparkles: feat: setup newUserValidation at POST:/users router

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import { newUserValidation } from '../middlewares/validation';
 
 const routers = Router();
 const userController = new UserController();
 
-routers.post('/users', userController.newUser);
+routers.post('/users', newUserValidation, userController.newUser);
 
 export default routers;
