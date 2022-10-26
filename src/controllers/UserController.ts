2449241e0ba39ec3b0c58581b9deff8ff478e2e6
Adriano Costa <adrianofacosta@gmail.com>
2022-10-25 21:02:02 -0300

    :sparkles: feat: made verifyLogin method at UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,11 +3,22 @@ import statusCodes from '../utils/statusCode';
 import UserService from '../services/UserService';
 
 export default class UserController {
-  constructor(private userService = new UserService()) { }
+  constructor(private userService = new UserService()) {}
 
   public newUser = async (req: Request, res: Response) => {
     const user = req.body;
     const auth = await this.userService.createUser(user);
     res.status(statusCodes.CREATED).json({ token: auth });
+  };
+
+  public login = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    const auth = await this.userService.verifyLogin({ username, password });
+    if (!auth) {
+      return res
+        .status(statusCodes.UNAUTHORIZED)
+        .json({ message: 'Username or password invalid' });
+    }
+    res.status(statusCodes.OK).json({ token: auth });
   };
 }
