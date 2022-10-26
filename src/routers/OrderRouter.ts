9a86b798ce734f6d74fe298899ebff55e60df756
Adriano Costa <adrianofacosta@gmail.com>
2022-10-26 16:46:16 -0300

    :sparkles: feat: create POST:/orders route

--- src/routers/OrderRouter.ts
+++ src/routers/OrderRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import OrderController from '../controllers/OrderController';
+import authMiddlware from '../middlewares/authMiddleware';
+import { newOrderValidation } from '../middlewares/validation';
 
 const routers = Router();
 const orderController = new OrderController();
 
 routers.get('/orders', orderController.getAll);
+routers.post('/orders', authMiddlware, newOrderValidation, orderController.create);
 
 export default routers;
