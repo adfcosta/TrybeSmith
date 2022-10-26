5f8557835fb4a4488cfc3be3d5541d005c06eb3c
Adriano Costa <adrianofacosta@gmail.com>
2022-10-26 01:58:12 -0300

    :recycle: refactor: update generateToken method to include id to generate token

--- src/utils/JWT.ts
+++ src/utils/JWT.ts
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
+import { UserToken } from '../interfaces/User';
 
 export default class Token {
   TOKEN_SECRET = process.env.JWT_SECRET as string;
 
-  public generateToken = (username: string) => {
-    const payload = { username };
+  public generateToken = ({ username, id }: UserToken) => {
+    const payload = { username, id };
     const token = jwt.sign(payload, this.TOKEN_SECRET);
     return token;
   };
